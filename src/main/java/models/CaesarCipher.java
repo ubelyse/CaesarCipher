89a4bfae0e782a23840b3df800e954b40ee8d29f
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-11-14 18:04:03 +0200

    added the comment to some line

--- src/main/java/models/CaesarCipher.java
+++ src/main/java/models/CaesarCipher.java
@@ -13,13 +13,14 @@ public class CaesarCipher {
     public static char RotateEndofAlphabet(char y,int a){
 
         final int lengthofalphabets = 26;
-        final char lettershift = Character.isUpperCase(y) ? 'A' : 'a';
+        //ascii has different codes according to letter so this line is checking whether letter is upper or lower case
+        final char asciiShift = Character.isUpperCase(y) ? 'A' : 'a';
         final int caesarciphershift = a % lengthofalphabets;
 
-
-        char shifted = (char) (y - lettershift);
+        char shifted = (char) (y - asciiShift);
+        // this line is for rotating the letter
         shifted = (char) ((shifted + caesarciphershift + lengthofalphabets) % lengthofalphabets);
-        return (char) (shifted + lettershift);
+        return (char) (shifted + asciiShift);
     }
 
 }
